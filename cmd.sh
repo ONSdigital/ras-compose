fd95e7c95eec23b77406fba98914f571943799b7
David Carboni <david@carboni.io>
2017-02-21 13:15:47 +0000

    Programmatically getting the name of the Docker network to attach the container to.

--- cmd.sh
+++ cmd.sh
@@ -5,6 +5,9 @@
 docker build --tag cmd curl/
 # docker build --no-cache --tag cmd curl/
 
+# Get the network name:
+network=`docker network ls --filter name=ras -q`
+
 # Start an interactive shell in a container connected to the network that docker-compose will create
 echo - You should be able to run curl commands from here to the components.
-docker run -it --rm --name cmd --net rascompose_ras cmd
+docker run -it --rm --name cmd --net $network cmd
